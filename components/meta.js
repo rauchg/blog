69fa3ef509b97ab86dfcefb46def1de7758e7586
Guillermo Rauch <rauchg@gmail.com>
2017-03-28 21:55:57 -0700

    boom

--- components/meta.js
+++ components/meta.js
@@ -1,4 +1,10 @@
 import Head from 'next/head'
+import NProgress from 'nprogress'
+import Router from 'next/router'
+
+Router.onRouteChangeStart = () => NProgress.start()
+Router.onRouteChangeComplete = () => NProgress.done()
+Router.onRouteChangeError = () => NProgress.done()
 
 export default () => (
   <div>
@@ -26,6 +32,32 @@ export default () => (
         color: #fff;
         background: #22BAD9;
         text-decoration: none;
+      }
+
+      /* loading progress bar styles */
+      #nprogress {
+        pointer-events: none;
+      }
+
+      #nprogress .bar {
+        background: #22BAD9;
+        position: fixed;
+        z-index: 1031;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 2px;
+      }
+
+      #nprogress .peg {
+        display: block;
+        position: absolute;
+        right: 0px;
+        width: 100px;
+        height: 100%;
+        box-shadow: 0 0 10px #22BAD9, 0 0 5px #22BAD9;
+        opacity: 1.0;
+        transform: rotate(3deg) translate(0px, -4px);
       }
     `}</style>
   </div>
