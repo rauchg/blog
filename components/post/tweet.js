ebd15fbf5184aab235c40e948ba1a0b10a7a0711
Luis Alvarez <luis@vercel.com>
2020-06-03 10:48:36 -0500

    Remove twitter embed

--- components/post/tweet.js
+++ components/post/tweet.js
@@ -1,70 +1,31 @@
 import { useRouter } from 'next/router';
-import { TwitterTweetEmbed } from 'react-twitter-embed';
 import { useTweet } from '../../lib/tweets';
 import Node from '../../static-tweet/components/html/node';
 import components from '../../static-tweet/components/twitter-layout/components';
 import twitterTheme from '../../static-tweet/components/twitter-layout/twitter.module.css';
 
 export default function Tweet({ id, caption }) {
-  const ast = useTweet(id);
+  const tweet = useTweet(id);
 
-  if (ast) {
-    return (
-      <main className={twitterTheme.theme}>
-        <Node components={components} node={ast[0]} />
-
-        <style jsx>{`
-          main {
-            max-width: 500px;
-            min-width: 220px;
-            margin: 2rem auto;
-          }
-          @media (max-width: 600px) {
-            main {
-              max-width: 300px;
-            }
-          }
-        `}</style>
-      </main>
-    );
-  }
+  // Happens when `getStaticProps` is traversing the tree to collect the tweet ids
+  if (tweet.ignore) return null;
 
   return (
-    <main>
-      <div>
-        <TwitterTweetEmbed tweetId={id} />
-      </div>
-      {caption != null ? <p>{caption}</p> : null}
+    <main className={twitterTheme.theme}>
+      <Node components={components} node={tweet.ast[0]} />
 
       <style jsx>{`
         main {
-          margin: 30px 0;
+          max-width: 500px;
+          min-width: 220px;
+          margin: 2rem auto;
         }
-
-        p {
-          font-size: 14px;
-          color: #999;
-          text-align: center;
-          margin: 0;
-          margin-top: 10px;
-          padding: 0;
-        }
-
-        main :global(.twitter-tweet) {
-          margin: auto !important;
-        }
-
         @media (max-width: 600px) {
-          main :global(.twitter-tweet) {
-            max-width: 300px !important;
-            width: 300px !important;
-            position: relative;
-            left: 0 !important;
+          main {
+            max-width: 300px;
           }
         }
       `}</style>
     </main>
   );
 }
-
-Tweet.tweets = [];
