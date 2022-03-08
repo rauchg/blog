4e3b0447463cdc97c9e2df594005e40425b2f632
QiwenWang <qiwen.wang@marvelstonemed.com>
2022-03-08 13:01:05 +0800

    test body

--- css/typography.js
+++ css/typography.js
@@ -1,9 +1,19 @@
 import css from "styled-jsx/css";
 
 const style = css.global`
+  // body {
+  //   font-family: -apple-system, BlinkMacSystemFont, sans-serif;
+  //   -webkit-font-smoothing: antialiased;
+  // }
+
   body {
-    font-family: -apple-system, BlinkMacSystemFont, sans-serif;
-    -webkit-font-smoothing: antialiased;
+    background: #fff url(/images/body-bg.jpg) center top no-repeat;
+    color: #424242;
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 0.8125em;
+    line-height: 1.5385;
+    margin: 0;
+    padding: 0;
   }
 
   a[href] {
