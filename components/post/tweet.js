0e1ba7a208d5ba253fa00baf0ce7bad6a7427c09
Guillermo Rauch <rauchg@gmail.com>
2022-06-09 17:17:06 -0700

    refactor static tweets to use package

--- components/post/tweet.js
+++ components/post/tweet.js
@@ -1,24 +1,30 @@
-import { useRouter } from "next/router";
 import { useTweet } from "../../lib/tweets";
-import Node from "../../static-tweet/components/html/node";
-import components from "../../static-tweet/components/twitter-layout/components";
-import twitterTheme from "../../static-tweet/components/twitter-layout/twitter.module.css";
+import { Tweet as ChildTweet } from "react-static-tweets";
 
-export default function Tweet({ id, br, caption }) {
+export default function Tweet({ id, caption }) {
   const tweet = useTweet(id);
 
-  // Happens when `getStaticProps` is traversing the tree to collect the tweet ids
-  if (tweet.ignore) return null;
+  if (tweet.ignore) {
+    // this is the "collection" phase (`getTweets`)
+    // where the renderer gets the list of tweets that will
+    // later be rendered. This is how we "hack around" not
+    // yet having Suspense based component-level data fetching
+    return null;
+  }
 
   return (
-    <main className={twitterTheme.theme}>
-      <Node components={components} node={tweet.ast[0]} br={br} />
+    <main>
+      <ChildTweet id={id} ast={tweet.ast} />
       {caption != null ? <p>{caption}</p> : null}
       <style jsx>{`
         main {
           max-width: 100%;
           min-width: 220px;
           margin: 2rem auto;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          flex-direction: column;
         }
         p {
           font-size: 14px;
--- static-tweet/lib/format-number.js
+++
@@ -1,3 +0,0 @@
-export default function formatNumber(n) {
-  return n.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
-}
