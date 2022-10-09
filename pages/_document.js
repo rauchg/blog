c70d9dd56db6dfbda81ba3a5e232d4931fb3b67d
Animesh Koratana <koratana@playerzero.app>
2022-10-08 21:36:22 -0400

    Added PlayerZero script to the blog main page

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -7,6 +8,10 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Script
+          src="https://go.playerzero.app/record/63421d43b6d22874d9d633eb"
+          strategy="afterInteractive"
+        ></Script>
       </body>
     </Html>
   );
