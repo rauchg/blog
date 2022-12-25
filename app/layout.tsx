209dec138411476c3d66cbe164c2f23cc3af5a8d
Guillermo Rauch <rauchg@gmail.com>
2022-12-25 14:29:51 -0800

    improve

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`${inter.className}`}>
+    <html
+      lang="en"
+      className={`${inter.className}`}
+      suppressHydrationWarning={true}
+    >
       <head>
         <script
           dangerouslySetInnerHTML={{
