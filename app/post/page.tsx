f4f370599cde3fb5118b85f39a1d4c1ee5ab2d26
Guillermo Rauch <rauchg@gmail.com>
2022-12-25 21:29:21 -0800

    blog post

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -1,7 +1,9 @@
 export default () => {
   return (
-    <>
-      <h1 className="text-2xl font-bold mb-1">Making the Web. Faster.</h1>
+    <article className="text-gray-300">
+      <h1 className="text-2xl font-bold mb-1 text-gray-100">
+        Making the Web. Faster.
+      </h1>
       <p className="font-mono text-xs text-gray-500">
         <span>
           <a href="#">@rauchg</a>
@@ -13,8 +15,15 @@ export default () => {
       </p>
 
       <p className="my-6">
-        Following our Series B last December, I&rsquo;m happy to announce
-        we&rsquo;ve raised a $102M Series C from existing and new investors.
+        Following our{" "}
+        <a href="#" className="text-white border-b border-1 border-gray-500">
+          Series B last December
+        </a>
+        , I&rsquo;m happy to announce we&rsquo;ve raised a{" "}
+        <a href="#" className="text-white border-b border-1 border-gray-500">
+          <b>$102M Series C</b>
+        </a>{" "}
+        from existing and new investors.
       </p>
 
       <p className="my-6">
@@ -26,15 +35,23 @@ export default () => {
 
       <ol className="my-6 list-decimal list-inside">
         <li className="my-1">
-          <a href="#">Build the SDK for Web</a>
+          <a href="#" className="text-white border-b border-1 border-gray-500">
+            <b>Build the SDK for Web</b>
+          </a>
         </li>
         <li className="my-1">
-          <a href="#">Lower the barrier of entry</a>
+          <a href="#" className="text-white border-b border-1 border-gray-500">
+            <b>Lower the barrier of entry</b>
+          </a>
         </li>
         <li className="my-1">
-          <a href="#">Focus on the end-user</a>
+          <a href="#" className="text-white border-b border-1 border-gray-500">
+            <b>Focus on the end-user</b>
+          </a>
         </li>
       </ol>
-    </>
+
+      <h2 className="font-bold text-xl my-10">1. Build the SDK for Web</h2>
+    </article>
   );
 };
