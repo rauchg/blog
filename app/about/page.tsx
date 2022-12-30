779dc44a02dc85b121d3c342b569dbb55e72ca8b
Guillermo Rauch <rauchg@gmail.com>
2022-12-30 10:30:41 -0800

    improve

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,7 +9,7 @@ export default function About() {
         <Image
           src="/images/rauchg-3d4cecf.jpg"
           alt="Guillermo Rauch"
-          className="rounded-full bg-gray-100 block my-5 mx-auto sm:float-right sm:ml-5 sm:mb-5 grayscale hover:grayscale-0"
+          className="rounded-full bg-gray-100 block mt-2 mb-5 mx-auto sm:float-right sm:ml-5 sm:mb-5 grayscale hover:grayscale-0"
           unoptimized
           width={160}
           height={160}
