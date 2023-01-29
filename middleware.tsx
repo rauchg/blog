d6b38b5bf9f4609339cce54bfc5450cb13395e67
Guillermo Rauch <rauchg@gmail.com>
2023-01-28 20:13:51 -0800

    simplify middleware

--- middleware.tsx
+++ middleware.tsx
@@ -1,32 +1,9 @@
 import { NextResponse } from "next/server";
-import links from "./links.json";
-import type { NextRequest } from "next/server";
 
 let initialDate = Date.now();
 
-export default async function middleware(req: NextRequest) {
+export default async function middleware() {
   const next = NextResponse.next();
   next.headers.set("x-edge-age", String(Date.now() - initialDate));
-
-  const url = new URL(req.nextUrl);
-  const linkId = url.pathname.split("/")[2];
-  const link = links[linkId];
-
-  if (!linkId) {
-    const notFound = new URL("/404", req.nextUrl);
-    return NextResponse.rewrite(notFound);
-  }
-
-  if (
-    url.searchParams.get("bot") ||
-    /bot/i.test(req.headers.get("user-agent"))
-  ) {
-    return next;
-  } else {
-    return NextResponse.redirect(link.link);
-  }
+  return next;
 }
-
-export const config = {
-  matcher: "/links/:path",
-};
