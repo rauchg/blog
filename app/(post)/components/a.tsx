97ec5df9da252dc9d42dc97627de4f6985cae08e
Guillermo Rauch <rauchg@gmail.com>
2023-02-27 19:15:25 -0800

    fix link transitions

--- app/(post)/components/a.tsx
+++ app/(post)/components/a.tsx
@@ -1,10 +1,13 @@
-export function A({ children, className = "", ...props }) {
+import Link from "next/link";
+
+export function A({ children, className = "", href, ...props }) {
   return (
-    <a
+    <Link
+      href={href}
       className={`border-b text-gray-600 border-gray-300 transition-[border-color] hover:border-gray-600 dark:text-white dark:border-gray-500 dark:hover:border-white ${className}`}
       {...props}
     >
       {children}
-    </a>
+    </Link>
   );
 }
