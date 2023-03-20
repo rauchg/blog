455aa9911c64910a71a83cc24f72bd0bb35ca96c
Guillermo Rauch <rauchg@gmail.com>
2023-03-19 19:32:54 -0700

    add og for homepage

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,23 @@ import { getPosts } from "./get-posts";
 
 export const revalidate = 1;
 
+export const metadata = {
+  openGraph: {
+    title: "Guillermo Rauchg's blog",
+    description:
+      "Guillermo Rauch is the CEO and founder of Vercel, a software engineer, and the creator of Next.js, Mongoose, Socket.io and other open source libraries.",
+    url: "https://rauchg.com",
+    siteName: "Guillermo Rauchg's blog",
+    images: [
+      {
+        url: "/og/main",
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+};
+
 export default async function Home() {
   const posts = await getPosts();
   return <Posts posts={posts} />;
