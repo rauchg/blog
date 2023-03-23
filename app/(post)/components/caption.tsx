40212c9159ddc70e558057d6ae0ff0aa15cdc43d
Guillermo Rauch <rauchg@gmail.com>
2023-03-23 16:09:01 -0700

    fix text balancing

--- app/(post)/components/caption.tsx
+++ app/(post)/components/caption.tsx
@@ -3,7 +3,7 @@ import type { ReactNode } from "react";
 
 export function Caption({ children }: { children: ReactNode }) {
   return (
-    <span className="block text-xs my-3 font-mono text-gray-500 text-center leading-normal">
+    <span className="block w-full text-xs my-3 font-mono text-gray-500 text-center leading-normal">
       <Balancer>
         <span className="[&>a]:post-link">{children}</span>
       </Balancer>
