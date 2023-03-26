5c565ec1c1036aab3c44c74809e624a56c5ce613
Guillermo Rauch <rauchg@gmail.com>
2023-03-26 19:07:30 -0400

    improve view api

--- app/api/view/route.ts
+++ app/api/view/route.ts
@@ -1,30 +1,39 @@
 export const runtime = "edge";
 
-import convexConfig from "@/convex.json";
+import redis from "@/app/redis";
+import postsData from "@/app/posts.json";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function GET(req: NextRequest) {
   const url = new URL(req.url);
-  const id = url.searchParams.get("id");
+  const id = url.searchParams.get("id") ?? null;
 
-  if (!id) {
-    return NextResponse.json({ error: 'Missing "id" query' }, { status: 400 });
+  if (id === null) {
+    return NextResponse.json(
+      {
+        error: {
+          message: 'Missing "id" query',
+          code: "MISSING_ID",
+        },
+      },
+      { status: 400 }
+    );
   }
 
-  await fetch(`${convexConfig.prodUrl}/api/0.1.4/udf`, {
-    method: "POST",
-    body: JSON.stringify({
-      path: "addView",
-      args: [process.env.CONVEX_AUTH_SECRET, id],
-      tokens: [],
-    }),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  if (!postsData.posts.find(post => post.id === id)) {
+    return NextResponse.json(
+      {
+        error: {
+          message: "Unknown post",
+          code: "UNKNOWN_POST",
+        },
+      },
+      { status: 400 }
+    );
+  }
 
-  return NextResponse.json({
-    ok: true,
-  });
+  await redis.hincrby("views", id, 1);
+
+  return NextResponse.json({ ok: true });
 }
