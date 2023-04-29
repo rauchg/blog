1db5486bd18cee7ee75804f8e8b8c76ac63be782
Guillermo Rauch <rauchg@gmail.com>
2023-04-28 18:37:32 -0700

    revalidate home

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { Posts } from "./posts";
 import { getPosts } from "./get-posts";
 
+export const revalidate = 60;
+
 export default async function Home() {
   const posts = await getPosts();
   return <Posts posts={posts} />;
