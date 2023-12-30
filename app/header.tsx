b471d640e491a2fbdbb01c116d60cb0bc27582b6
Josh Oynick <josh.oynick@vercel.com>
2023-12-30 14:51:00 -0800

    Update header.tsx
    Replaced twitter bird logo with X logo, and make logo only appear if inline (goal is to avoid overflow on mobile)

--- app/header.tsx
+++ app/header.tsx
@@ -21,7 +21,7 @@ export function Header() {
           target="_blank"
           className="inline-flex hover:bg-gray-200 dark:hover:bg-[#313131] active:bg-gray-300 dark:active:bg-[#242424] items-center p-2 rounded-sm transition-[background-color] whitespace-nowrap -mr-2"
         >
-          <TweetIcon style={{ marginRight: 4 }} /> Follow{" "}
+          <TweetIcon className="hidden md:inline" style={{ marginRight: 3 }} /> Follow{" "}
           <span className="hidden md:inline">&nbsp;me</span>
         </a>
       </nav>
@@ -43,8 +43,8 @@ function TweetIcon(props: any) {
         fillRule="nonzero"
         stroke="none"
         strokeWidth={1}
-        d="M8.28 20.26c7.55 0 11.68-6.26 11.68-11.67v-.53c.8-.58 1.49-1.3 2.04-2.13-.74.33-1.53.54-2.36.65.85-.5 1.5-1.32 1.8-2.28-.78.48-1.66.81-2.6 1a4.1 4.1 0 0 0-7 3.74c-3.4-.17-6.43-1.8-8.46-4.29a4.1 4.1 0 0 0 1.28 5.48c-.68-.02-1.3-.2-1.86-.5v.05a4.11 4.11 0 0 0 3.29 4.02 4 4 0 0 1-1.85.08 4.1 4.1 0 0 0 3.83 2.85A8.23 8.23 0 0 1 2 18.43a11.67 11.67 0 0 0 6.28 1.83"
-      />
+        d="M178.57 127.15 290.27 0h-26.46l-97.03 110.38L89.34 0H0l117.13 166.93L0 300.25h26.46l102.4-116.59 81.8 116.59h89.34M36.01 19.54H76.66l187.13 262.13h-40.66"
+        />
     </svg>
   );
 }
