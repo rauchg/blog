b37fe68f0553764d91e39d3c79565d99be12180d
Guillermo Rauch <rauchg@gmail.com>
2024-05-14 20:58:37 -0700

    test

--- middleware.tsx
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-let initialDate = Date.now();
-
-export default async function middleware() {
-  const next = NextResponse.next();
-  next.headers.set("x-edge-age", String(Date.now() - initialDate));
-  return next;
-}
