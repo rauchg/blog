927d3d7ac780c41c447b77415d324bd834823b78
Guillermo Rauch <rauchg@gmail.com>
2024-11-12 17:10:51 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,7 +52,7 @@ export default function RootLayout({
         />
       </head>
 
-      <body className="dark:text-gray-100 max-w-2xl m-auto" style={{background: 'red'}}>
+      <body className="dark:text-gray-100 max-w-2xl m-auto">
         <main className="p-6 pt-3 md:pt-6 min-h-screen">
           <Header />
           {children}
