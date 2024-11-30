a6858f40e0aca641c76b65aa3e0714f58199f21b
Tobias Lins <me@tobi.sh>
2024-11-30 21:16:20 +0100

    Add speed insights and use new @vercel/analytics for next

--- app/analytics.tsx
+++ app/analytics.tsx
@@ -1,6 +1,12 @@
 "use client";
-import { Analytics as AnalyticsComponent } from "@vercel/analytics/react";
+import { Analytics as AnalyticsComponent } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export function Analytics() {
-  return <AnalyticsComponent />;
+  return (
+    <>
+      <AnalyticsComponent />
+      <SpeedInsights />
+    </>
+  );
 }
