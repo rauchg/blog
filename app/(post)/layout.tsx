0896c46151801a3d47262469fef09f72a694e65c
Ron Mashate <rmashate@gmail.com>
2025-11-08 13:27:21 -0500

    Fix: Remove duplicate Header from blog post layout

--- app/(post)/layout.tsx
+++ app/(post)/layout.tsx
@@ -1,4 +1,3 @@
-import Header from "../header";
 import Footer from "../footer";
 import "../globals.css";
 
@@ -10,7 +9,6 @@ export default function PostLayout({
   return (
     <div className="antialiased max-w-2xl flex flex-col md:flex-row mx-4 lg:mx-auto">
       <main className="flex-auto min-w-0 mt-12 md:mt-0 flex flex-col px-2 md:px-0">
-        <Header />
         <article className="prose prose-neutral dark:prose-invert">
           {children}
         </article>
