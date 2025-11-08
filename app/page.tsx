69bb99776081081a00484dbb1a2021e728fc1460
Ron Mashate <rmashate@gmail.com>
2025-11-08 12:00:21 -0500

    Fix: Update page.tsx to fetch and pass posts prop correctly

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,10 @@
-import Posts from "./posts";
-import Header from "./header";
-import Footer from "./footer";
+import { Posts } from "./posts";
+import { getPosts } from "./get-posts";
 import "./globals.css";
 
-export default function HomePage() {
-  return (
-    <div className="antialiased max-w-2xl flex flex-col md:flex-row mx-4 lg:mx-auto">
-      <main className="flex-auto min-w-0 mt-12 md:mt-0 flex flex-col px-2 md:px-0">
-        <Header />
-        <Posts />
-        <Footer />
-      </main>
-    </div>
-  );
+export const revalidate = 300;
+
+export default async function Home() {
+  const posts = await getPosts();
+  return <Posts posts={posts} />;
 }
