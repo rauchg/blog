2ba4380bbdd587d9a4d992d14b9e448d3a773127
Ron Mashate <rmashate@gmail.com>
2025-11-08 11:28:28 -0500

    Fix Footer default export

--- app/footer.tsx
+++ app/footer.tsx
@@ -4,17 +4,19 @@ export function Footer() {
   return (
     <footer className="p-6 pt-3 pb-6 flex text-xs text-center mt-3 dark:text-gray-400 text-gray-500 font-mono">
       <div className="grow text-left">
-        Guillermo Rauch (
-        <A target="_blank" href="https://twitter.com/rauchg">
-          @rauchg
+        Ron Mashate (
+        <A target="_blank" href="https://twitter.com/rmashate">
+          @rmashate
         </A>
         )
       </div>
       <div>
-        <A target="_blank" href="https://github.com/rauchg/blog">
+        <A target="_blank" href="https://github.com/rmashate/blog">
           Source
         </A>
       </div>
     </footer>
   );
 }
+
+export default Footer;
